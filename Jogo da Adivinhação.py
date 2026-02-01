6949d31a2755dfc7e65f8b833151cc38c0241719
Marcelo Celoca <marceloceloca@gmail.com>
2026-02-01 15:43:03 -0300

    Subindo arquivo .py

---
+++ Jogo da Adivinhação.py
@@ -0,0 +1,20 @@
+import random
+
+cont = 0
+num_secreto = random.randint(1, 10)
+nome = str(input('Olá! Qual o seu nome? '))
+while True:
+    num_user = int(input(f'Digite seu palpite, \033[35m{nome}\033[m (números de 1 a 10): '))
+    cont += 1
+    if num_user == num_secreto:
+        print(f'\033[34mParabéns!\033[m Você acertou o número com {cont} tentativas.')
+        break
+    elif num_user < num_secreto:
+        print('Mais alto!')
+    else:
+        print('Mais baixo!')
+        
+        
+
+
+
